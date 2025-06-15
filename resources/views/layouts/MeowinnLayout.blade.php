1a7b8c4718f6754128651074f2c72509de02a256
TsaqifRadhitya <tsaqifradhitya@gmail.com>
2025-06-16 02:04:34 +0700

    add meta tag

--- resources/views/layouts/MeowinnLayout.blade.php
+++ resources/views/layouts/MeowinnLayout.blade.php
@@ -6,6 +6,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Meowinn - {{ $header }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script>
--- resources/views/layouts/PetHouseLayout.blade.php
+++ resources/views/layouts/PetHouseLayout.blade.php
@@ -4,6 +4,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>pethouse - {{ $header }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
